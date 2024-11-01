3d9b98f970271824130ea274434e4aa9979d8909
pzy <pzyllll@outlook.com>
2024-11-01 13:22:23 +0800

    111

--- lab03_6/src/App.vue
+++ lab03_6/src/App.vue
@@ -1,30 +1,106 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+//import { RouterLink, RouterView } from 'vue-router'
+//import HelloWorld from './components/EventCard.vue'
+import { ref, watchEffect } from 'vue';
+import { RouterLink, useRouter, useRoute } from 'vue-router';
+const router = useRouter();
+const route = useRoute();
+
+const itemsPerPage = ref(5); // 默认每页显示5条记录
+
+
+watchEffect(() => {
+  if (route.query.itemsPerPage) {
+    itemsPerPage.value = parseInt(route.query.itemsPerPage as string, 10);
+  }
+});
+
+function navigateWithPageSize(size: number) {
+  router.push({ path: '/', query: { ...route.query, itemsPerPage: size } });
+}
+
+
+
+
+
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+<div id="layout">
+    <header>
+      <div class="wrapper">
+        <nav>
+          <RouterLink to="/">Event</RouterLink> |
+          <RouterLink to="/about">About</RouterLink> |
+          <RouterLink to="/student">Student</RouterLink>
+          <div>
+            <button class="page-size-btn" @click="navigateWithPageSize(3)">Page Size: 3</button>
+          <button class="page-size-btn" @click="navigateWithPageSize(4)">Page Size: 4</button>
+          <button class="page-size-btn" @click="navigateWithPageSize(5)">Page Size: 5</button>
+        </div>
+        </nav>
+      </div>
+    </header>
+  
+
+    <RouterView :itemsPerPage="itemsPerPage" />  </div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<style >
+
+#layout {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+   text-align: center;
+   color: #2c3e50;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+nav {
+  padding: 30px;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+
 }
+
+nav a.router-link-exact-active {
+  color: #42b983;
+ }
+
+h2 {
+  font-size: 20px;
+}
+
+
+.button-group {
+  display: flex;
+  justify-content: center;
+  gap: 10px;
+  margin-top: 20px;
+}
+
+.page-size-btn {
+  padding: 5px 10px;
+  font-size: 14px;
+  cursor: pointer;
+  border: 1px solid #42b983;
+  background-color: white;
+  color: #42b983;
+  transition: all 0.3s ease;
+}
+
+.page-size-btn.active {
+  background-color: white;
+  color: green;
+}
+
+.page-size-btn:hover {
+  background-color: white;
+  color: green;
+}
+
 </style>

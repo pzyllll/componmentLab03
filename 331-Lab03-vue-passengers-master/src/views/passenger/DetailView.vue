655fe3fd6517aba54e6448c94b9b5a7b88fd8451
pzy <pzyllll@outlook.com>
2024-11-05 13:10:44 +0800

    changdetail

--- 331-Lab03-vue-passengers-master/src/views/passenger/DetailView.vue
+++ 331-Lab03-vue-passengers-master/src/views/passenger/DetailView.vue
@@ -1,32 +1,62 @@
 <script setup lang="ts">
-import { toRefs, defineProps,onMounted, watch } from 'vue'
-import { type Data, type PassengerData } from '@/types'
+import { ref, onMounted, watch } from 'vue';
 import { useRoute } from 'vue-router';
 import PassengerServices from '@/services/PassengerServices';
+import type { PassengerData } from '@/types';
 import FlashMessage from '@/components/FlashMessage.vue';
 
 const route = useRoute();
 const passenger = ref<PassengerData | null>(null);
 const showFlashMessage = ref(false);
 const flashMessage = ref('');
-const props = defineProps<{
-    data: Data
-    id: string
-}>()
-const { data } = toRefs(props)
 
+onMounted(async () => {
+  try {
+    const response = await PassengerServices.getPassenger(route.params.id as string);
+    passenger.value = response.data;
+  } catch (error) {
+    console.error('Passenger not found', error);
+  }
+
+  // Check if there is a flash message in the route query
+  if (route.query.flashMessage) {
+    showFlashMessage.value = true;
+    flashMessage.value = route.query.flashMessage as string;
+  }
+});
+
+// Watch for changes in the route query to handle flash messages
+watch(() => route.query.flashMessage, (newFlashMessage) => {
+  if (newFlashMessage) {
+    showFlashMessage.value = true;
+    flashMessage.value = newFlashMessage as string;
+  }
+});
+
+const closeFlashMessage = () => {
+  showFlashMessage.value = false;
+};
 </script>
+
 <template>
-  <div v-if="data">
-    <h1>{{ data.name }}</h1>
-    <p>{{ data.name }} has\ tripped {{ data.trips }} times</p>
+  <div v-if="passenger">
+    <h1>{{ passenger.name }}</h1>
+    <p>{{ passenger.name }} has tripped {{ passenger.trips }} times</p>
     <h2>Airlines</h2>
     <ul>
-      <li v-for="airline in data.airline" :key="airline._id">
-        <router-link :to="{ name: 'airline-detail-view', params: { id: $route.params.id, airlineId: airline._id } }">
+      <li v-for="airline in passenger.airline" :key="airline._id">
+        <router-link :to="{ name: 'airline-detail-view', params: { id: route.params.id, airlineId: airline._id } }">
           {{ airline.name }}
         </router-link>
       </li>
     </ul>
+    <FlashMessage v-if="showFlashMessage" :message="flashMessage" @close="closeFlashMessage" />
+  </div>
+  <div v-else>
+    <p>Passenger not found.</p>
   </div>
 </template>
+
+<style scoped>
+/* Add any additional styles here */
+</style>
